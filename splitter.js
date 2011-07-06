3f1c24cb670f99bba43a57b443e24c0dfa271e12
Leif Wickland <leifwickland@gmail.com>
2011-07-06 14:37:50 -0600

    Figured out how to detected a dropped listener in the most roundabout way imaginable.

--- splitter.js
+++ splitter.js
@@ -40,15 +40,46 @@ function urlToHttpOptions(url) {
 
 function dumpObjectIndented(obj, indent) { var result = ""; if (indent == null) indent = ""; for (var property in obj) { var value = obj[property]; if (typeof value == 'string') { value = "'" + value + "'"; } else if (typeof value == 'object') { if (value instanceof Array) { /* Just let JS convert the Array to a string! */ value = "[ " + value + " ]"; } else { /*  Recursive dump (replace "  " by "\t" or something else if you prefer) */ var od = dumpObjectIndented(value, indent + "  "); /* If you like { on the same line as the key value = "{\n" + od + "\n" + indent + "}"; If you prefer { and } to be aligned */ value = "\n" + indent + "{\n" + od + "\n" + indent + "}"; } } result += indent + "'" + property + "' : " + value + ",\n"; } return result.replace(/,\n$/, ""); }
 
+var responseStreams = [ ];
+var consecutiveFailedWrites = [];
+var failureThreshold = 1000;
+
+function recordFailedWrite(index) {
+  if ((consecutiveFailedWrites[index] += 1) > failureThreshold) {
+    responseStreams[index] = null;
+    console.log("Evicted listener " + index + " because it failed " + failureThreshold + " consecutive writes.");
+  }
+}
+
+function recordSuccessfulWrite(index) {
+  consecutiveFailedWrites[index] = 0;
+}
+
+var dataCount = 0;
 function urlToHttpRequest(url) {
   var req = http.request(urlToHttpOptions(url), function(res) {
     console.log('STATUS: ' + res.statusCode);
     console.log('HEADERS: ' + JSON.stringify(res.headers));
     res.setEncoding('utf8');
     res.on('data', function (data) {
-      console.log("Origin Data: " + data);
+      dataCount++;
+      //console.log("Origin Data: " + data);
       for (i in responseStreams) {
-        responseStreams[i].write(data, 'binary');
+        var stream = responseStreams[i];
+        if (stream == null) {
+          continue;
+        }
+        var writeResult = responseStreams[i].write(data, 'binary');
+        if (writeResult) {
+          recordSuccessfulWrite(i);
+        }
+        else {
+          recordFailedWrite(i);
+        }
+        if (dataCount % 100 == 0) {
+
+          console.log("DataCount: " + dataCount + "  listener " + i + " is writable: " + responseStreams[i].writable + ". WriteResult: " + writeResult + "  consecutiveFails: " + consecutiveFailedWrites[i]);
+        }
       }
     });
   });
@@ -71,14 +102,31 @@ function urlToHttpRequest(url) {
   return req;
 }
 
-var responseStreams = [ logStream ];
+addListener(logStream);
 
 function serverCallback(request, response) {
   console.log("Got a new listener");
-  responseStreams.push(response);
+  request.on('end', function() { console.log("request end: " + request) });
+  request.on('clone', function() { console.log("request clone: " + request) });
+  addListener(response)
+}
+
+var listenerCount = 0;
+function addListener(writeStream) {
+  var index = responseStreams.length
+  var name = "listener" + index;
+  writeStream.on('end', function() { console.log("writeStream end: " + name) });
+  writeStream.on('close', function() { console.log("writeStream close: " + name) });
+  writeStream.on('error', function(ex) { console.log("writeStream error: " + name + " ex: " + ex) });
+  writeStream.on('drain', function() { recordSuccessfulWrite(index); });
+  responseStreams.push(writeStream);
+  recordSuccessfulWrite(index);
 }
 
 console.log("Creating server listening on " + serverPort);
-http.createServer(serverCallback).listen(serverPort);
+var server = http.createServer(serverCallback);
+server.listen(serverPort);
+server.on('close', function(errno) { console.log("Server close: " + errno) } );
+server.on('clientError', function(exception) { console.log("Server clientError: " + exception) } );
 
 urlToHttpRequest(urlToSplit);
